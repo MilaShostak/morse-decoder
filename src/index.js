442fea47c5c48de6b704eb0f43323d15780b460a
MilaShostak <milashostak111@gmail.com>
2026-05-01 15:20:38 +0300

    feat: implement morse code decoder function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,33 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
-};
+function decode(expr) {
+  let result = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    const block = expr.substring(i, i + 10);
+
+    if (block === '**********') {
+      result += ' ';
+    } else {
+      let morseCode = '';
+      for (let j = 0; j < block.length; j += 2) {
+        const pair = block.substring(j, j + 2);
+        if (pair === '10') {
+          morseCode += '.';
+        } else if (pair === '11') {
+          morseCode += '-';
+        }
+      }
+
+      if (MORSE_TABLE[morseCode]) {
+        result += MORSE_TABLE[morseCode];
+      }
+    }
+  }
+
+  return result;
+}
+
+module.exports = decode;
